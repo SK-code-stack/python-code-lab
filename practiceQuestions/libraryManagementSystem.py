539a6b2354f1ab3a632dd4e0315bd699b926d10f
salman <salmankhan.cw@gmail.com>
2025-11-22 19:18:06 +0500

    library management system is started

---
+++ practiceQuestions/libraryManagementSystem.py
@@ -0,0 +1,24 @@
+# Library Management System –
+#Build a class Book (title, author, available_copies) and Library to:
+
+
+class book:
+    def __init__(self):
+        pass
+
+    def choice(self):
+        pass
+
+    def addBook(self):
+        pass
+
+    def borrowBook(self):
+        pass
+
+    def returnBook(self):
+        pass
+
+
+
+b = book()
+b.choice() 
